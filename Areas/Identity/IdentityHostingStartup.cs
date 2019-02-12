eb8b6fa12b878c928c15fa306ffa4fd47ce4a099
Cvetelin Momchilov <cecko04@hotmail.com>
2019-02-12 17:43:45 +0200

    Uploading v1.0
    Uploading Development University Project. A basic ASP.NET Core with Authentication, with few changes.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DevUniversity.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DevUniversity.Areas.Identity.IdentityHostingStartup))]
+namespace DevUniversity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DevUniversityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DevUniversityContextConnection")));
+
+                //services.AddDefaultIdentity<IdentityUser>()
+                 //   .AddEntityFrameworkStores<DevUniversityContext>();
+            });
+        }
+    }
+}
